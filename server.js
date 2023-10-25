c8d5b3e58c9ac42143056f08e8f64cde7a44b77f
nicolasib <design.nicolasib@gmail.com>
2023-10-24 23:10:16 -0300

    vish

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const admin = require('firebase-admin');
 
 // Inicializar o Firebase
 const firebaseConfig = {
-  apiKey: "process.env.AIzaSyCVPdDwvn2GnugJw2Fm3k_idn4mXQlsn1Y",
+  apiKey: "process.env.FIREBASE_API_KEY",
   authDomain: "financial-n.firebaseapp.com",
   projectId: "financial-n",
   storageBucket: "financial-n.appspot.com",
@@ -12,7 +12,21 @@ const firebaseConfig = {
   appId: "1:869482768612:web:b1e9101a527b4fe5f664f1"
 };
 
-admin.initializeApp(firebaseConfig);
+admin.initializeApp({
+  credential: admin.credential.cert({
+    "type": "service_account",
+    "project_id": "financial-n",
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-amojr%40financial-n.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  })
+});
 
 const db = admin.firestore();
 
